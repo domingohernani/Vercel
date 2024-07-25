5cabbe83fcda5fe5ed7476b34d277270adee92cc
domingohernani <hernani.domingo1@gmail.com>
2024-07-25 15:58:19 +0800

    fixed missing icons

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ const manifestForPlugin = {
     "favicon-32x32.png",
     "android-chrome-192x192.png",
     "android-chrome-512x512.png",
+    "android-chrome-maskable-192x192.png"
   ],
   manifest: {
     name: "Vaxcare",
@@ -45,37 +46,37 @@ const manifestForPlugin = {
     ],
     icons: [
       {
-        src: "android-chrome-192x192.png",
+        src: "icons/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "android-chrome-512x512.png",
+        src: "icons/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "android-chrome-192x192.png",
+        src: "icons/android-chrome-maskable-192x192.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "maskable",
       },
       {
-        src: "favicon-16x16.png",
+        src: "icons/favicon-16x16.png",
         sizes: "16x16",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "favicon-32x32.png",
+        src: "icons/favicon-32x32.png",
         sizes: "32x32",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "apple-touch-icon.png",
+        src: "icons/apple-touch-icon.png",
         sizes: "180x180",
         type: "image/png",
         purpose: "any",
